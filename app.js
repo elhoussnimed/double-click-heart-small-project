7c65e365be19fd7d06376b2db23acd25a86f2bd5
mohammed el houssni <91427616+elhoussnimed@users.noreply.github.com>
2022-11-23 18:41:35 +0100

    add files to github

---
+++ app.js
@@ -0,0 +1,27 @@
+const image = document.querySelector(".image");
+const likesNumber = document.querySelector(".likesNumber");
+
+let currentLikesNumber = 0;
+
+image.addEventListener("dblclick", (e) => {
+  likeImage(e);
+  likesNumberCounter();
+});
+
+const likeImage = (e) => {
+  let xPosition = e.clientX;
+  let yPosition = e.clientY;
+
+  const heart = document.createElement("i");
+  heart.classList.add("fa-solid");
+  heart.classList.add("fa-heart");
+  heart.style.cssText = `top:${yPosition - image.offsetTop}px; left:${
+    xPosition - image.offsetLeft
+  }px`;
+  image.append(heart);
+};
+
+const likesNumberCounter = () => {
+  currentLikesNumber++;
+  likesNumber.innerHTML = currentLikesNumber;
+};
